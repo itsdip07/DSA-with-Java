768056fd847bd279fdaf765661a62e9b4bf305a2
itsdip07 <maitidipayan07@gmail.com>
2026-05-01 20:57:41 +0530

    Initial commit - DSA with Java

---
+++ Lecture10/Arrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String[] args) {
+        // int marks[] = new int [3];
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int [size];
+
+        //input 
+
+        for(int i =0; i< size; i++) {
+            numbers[i] = sc.nextInt();
+        }
+
+        int x = sc.nextInt();
+
+        for(int i=0; i<numbers.length; i++) {
+            if (numbers[i] == x) {
+                System.out.println("x found in index :" + i);
+            }
+        }
+    }
+}
---
+++ Lecture4/DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 12;
+        while(i<11) {
+            System.out.println("Hey!");
+        }
+        do {
+            System.out.println("Hey bro!");
+        } while (i<11);
+    }    
+}

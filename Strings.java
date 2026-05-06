ba9c2666be365cfe79ec3a75cbbcb1e66b26ae2a
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-05-06 23:10:39 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String[] args) {
+        //String Declaration 
+        Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("Your Name is "+ name);
+        String name1 = "Dipayan";
+        String name2 = "Dipayan";
+
+        if(name1.compareTo(name2)== 0) {
+            System.out.println("Strings are equal");
+        }
+
+        // String name = "Dipayan";
+        // String fullName = "Dipayan Maiti";
+        // String sentence = "My Name is Dipayan Maiti.";
+
+
+    }
+}

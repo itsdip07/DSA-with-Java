d1b66800a3dc579db58b0291cd724df6aab6ce17
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-05-07 19:46:18 +0530

    Add files via upload

---
+++ Recursion1.java
@@ -0,0 +1,15 @@
+public class Recursion1 {
+    public static void printFib(int a, int b, int n){
+
+    }
+    }
+    public static void main(String[] args) {
+        int a = 0, b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 7; 
+        printFib(a, b, b);
+
+        
+    }
+}

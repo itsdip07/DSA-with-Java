3722cbea9bdc35fd342908a7b7881714007498ed
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-05-15 19:53:58 +0530

    Delete Lecture4 directory

--- Lecture4/Loops.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Loops {
-    public static void main(String[] args) {
-        for(int counter = 0; counter < 3; counter = counter + 1) {
-            System.out.println(counter);
-        }
-    }
-}
